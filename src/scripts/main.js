0ebb39aee39cb9a303f719e3cdef87e3c4cd7bac
Tiartha <tiartha10@gmail.com>
2017-05-30 16:52:42 +0700

    update theme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
   
-  cube = require('bespoke-theme-atomantic'),
+  cube = require('bespoke-theme-cube'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
